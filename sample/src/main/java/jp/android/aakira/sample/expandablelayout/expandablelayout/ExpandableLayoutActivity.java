4c049042fb8ac44918a36cd4de50ae298576d9d6
A.Akira <developer.a.akira@gmail.com>
2015-10-30 01:50:17 +0900

    Use listener adapter in sample.

--- sample/src/main/java/jp/android/aakira/sample/expandablelayout/expandablelayout/ExpandableLayoutActivity.java
+++ sample/src/main/java/jp/android/aakira/sample/expandablelayout/expandablelayout/ExpandableLayoutActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+
 import jp.android.aakira.sample.expandablelayout.R;
 
 public class ExpandableLayoutActivity extends AppCompatActivity implements View.OnClickListener {
@@ -33,8 +34,8 @@ public class ExpandableLayoutActivity extends AppCompatActivity implements View.
         mExpandButton = (Button) findViewById(R.id.expandButton);
         mMoveChildButton = (Button) findViewById(R.id.moveChildButton);
         mMoveChildButton2 = (Button) findViewById(R.id.moveChildButton2);
-        mMoveTopButton = (Button)findViewById(R.id.moveTopButton);
-        mSetCloseHeihgtButton  = (Button) findViewById(R.id.setCloseHeightButton);
+        mMoveTopButton = (Button) findViewById(R.id.moveTopButton);
+        mSetCloseHeihgtButton = (Button) findViewById(R.id.setCloseHeightButton);
         mExpandLayout = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout);
         mExpandButton.setOnClickListener(this);
         mMoveChildButton.setOnClickListener(this);
