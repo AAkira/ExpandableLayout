f0b10c759cb815a808507fd2557611fe6830eeed
A.Akira <developer.a.akira@gmail.com>
2016-02-21 02:11:50 +0900

    Add new module which is library-ui-test.

---
+++ library-ui-test/src/main/java/com/github/aakira/expandablelayout/uitest/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.github.aakira.expandablelayout.uitest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
